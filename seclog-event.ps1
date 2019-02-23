a27ed3c3c61b2d907f7227234af245acd296931c
Sam <drpong2@gmail.com>
2019-02-23 09:48:44 -0600

    Update seclog-event.ps1

--- seclog-event.ps1
+++ seclog-event.ps1
@@ -1,9 +1,15 @@
 $date = (get-date).adddays(-1)
 $logins = get-eventlog -logname security -instanceid 4624 -after $date
 $count = $logins.count
+$path = 'C:\scripts\seclogdata\'
+$filename = $date.ToShortDateString()
+$filename = $filename.replace('/','.')
+
+
+$writepath = $path + $filename + '.log'
 
 for ($i = 0; $i -le $count; $i++){
     if ($logins.message[$i] -like "*Logon Type:`t`t3*"){
-        $logins.message[$i]
+        out-file -filepath $writepath -inputobject $logins.message[$i] -append
         }
 }

2c1f6a7d7d7e590fd8d2c8d356b15e120df3253a
Martin <martins237@mvcc>
2019-07-30 10:58:00 -0500

    checks for existence of the file, then creates it.

--- Sophosfix.ps1
+++ Sophosfix.ps1
@@ -1,3 +1,9 @@
 <# Sophos fix for the network scanner#>
 
-set-itemproperty -path "HKCR:\AppID\{C092D533-8791-42F8-8EBE-DB116F79B4B7" -name "SntpService" -value "SophosNtpService"
+
+if(!(get-item "HKLM:\SOFTWARE\Classes\AppID\{C092D533-8791-42F8-8EBE-DB116F79B4B7}")){
+new-item -path "HKLM:\SOFTWARE\Classes\AppID\" -name "{C092D533-8791-42F8-8EBE-DB116F79B4B7}"
+new-itemproperty -path "HKLM:\SOFTWARE\Classes\AppID\{C092D533-8791-42F8-8EBE-DB116F79B4B7}" -name "SntpService" -value "SophosNtpService" -propertytype "string"
+}
+
+#get-content "$env:windir\Panther\MigLog.xml" | select-string "{C092D533-8791-42F8-8EBE-DB116F79B4B7}"

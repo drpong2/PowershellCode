6f84e30937afe66a10be76f66dbe0b2ddc30f7d5
Martin <martins237@mvcc>
2019-10-09 14:18:29 -0500

    cleaned up a bit

--- poormansDSC.ps1
+++ poormansDSC.ps1
@@ -1,9 +1,22 @@
 <# Poor Man's DSC
 Get list of installed windows features from one computer to install on another computer.
+
+Must be run from the server that you'd like to update.
 #>
 
 read-host -prompt "Please enter target computer"
-$installed = invoke-command $targetcomputer -scriptblock {get-windowsfeature | where {$_.installstate -eq "installed"}}
-foreach ($install in $installed){
-install-windowsfeature $install.name
+
+try{
+    test-netconnection $targetcomputer
+    $proceed = 1
+}
+catch{
+    "Connection Failed!"
+    $proceed = 0
+}
+if($proceed){
+    $installed = invoke-command $targetcomputer -scriptblock {get-windowsfeature | where {$_.installstate -eq "installed"}}
+    foreach ($install in $installed){
+    install-windowsfeature $install.name
+    }
 }
